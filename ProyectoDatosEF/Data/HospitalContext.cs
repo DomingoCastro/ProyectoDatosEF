74887a091aef7e2a3245689db854bbbaf50fb8c1
DomingoCastro <domingocastro1998@outlook.com>
2023-01-24 18:23:06 +0100

    Add project files.

---
+++ ProyectoDatosEF/Data/HospitalContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoDatosEF.Models;
+
+namespace ProyectoDatosEF.Data
+{
+    public class HospitalContext :DbContext
+    {
+        //NECESITAMOS UN CONSTRUCTOR OBLIGATORIO PARA PODER RECIBIR LA CADENA DE CONEXION
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options) { }
+
+        // DEBEMOS MAPEAR CADA CLASE MODEL DE LA BASE DE DATOS CON UN OBJETO
+        // DbSet. CADA TABLA DE LA BASE DE DATOS SERA UN DbSet CON UN MODEL.
+        //NOSOTROS HAREMOS LA CONSULTAS A DICHO DbSet
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
---
+++ ProyectoDatosEF/Repositories/RepositoryHospital.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoDatosEF.Data;
+using ProyectoDatosEF.Models;
+
+namespace ProyectoDatosEF.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        //CREAMOS NUESTROS METODOS PARA RECUPERAR DATOS
+        public List<Hospital> GetHospitales()
+        {
+            var consulta = from datos in this.context.Hospitales
+                           select datos;
+            return consulta.ToList();
+        }
+
+        //METODO PARA DEVOLVER UN SOLO HOSPITAL
+        public Hospital FindHospital(int id)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.IdHospital== id
+                           select datos;
+            // COMO SOLO DEVUELVE UN HOSPITAL PODEMOS UTILIZAR EL METODO .First() o .FirstOrDefault()
+            // First daria fallo en caso de no enonctraro y FirstOrDefault daria un numero 0 en caso de no encontrarlo
+            return consulta.FirstOrDefault();
+        }
+    }
+}
